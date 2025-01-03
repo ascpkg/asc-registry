0dcf2f26c23454a8eaf6a6d9331b2a8bc6c4ed16
Capric <capric8416@gmail.com>
2025-01-03 17:10:19 +0800

    flatten https://github.com/microsoft/vcpkg/commit/3379a3e from me@mixaill.tk at 2017-03-12 15:22:05 +0300

---
+++ ports/mpir-3-0-0-2-0/portfile.cmake
@@ -0,0 +1,86 @@
+include(vcpkg_common_functions)
+
+set(MPIR_VERSION 3.0.0)
+set(MPIR_HASH "c735105db8b86db739fd915bf16064e6bc82d0565ad8858059e4e93f62c9d72d9a1c02a5ca9859b184346a8dc64fa714d4d61404cff1e405dc548cbd54d0a88e")
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/mpir-${MPIR_VERSION})
+
+vcpkg_download_distfile(ARCHIVE_FILE
+    URLS "http://mpir.org/mpir-${MPIR_VERSION}.tar.bz2"
+    FILENAME "mpir-${MPIR_VERSION}.tar.bz2"
+    SHA512 ${MPIR_HASH}
+)
+vcpkg_extract_source_archive(${ARCHIVE_FILE})
+
+if(VCPKG_PLATFORM_TOOLSET MATCHES "v141*")
+    set(MSVC_VERSION 15)
+else()
+    set(MSVC_VERSION 14)
+endif()
+vcpkg_get_windows_sdk(WINDOWS_SDK)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+	vcpkg_build_msbuild(
+		PROJECT_PATH ${SOURCE_PATH}/build.vc${MSVC_VERSION}/dll_mpir_gc/dll_mpir_gc.vcxproj
+		TARGET_PLATFORM_VERSION ${WINDOWS_SDK}
+	)
+else()
+	vcpkg_build_msbuild(
+		PROJECT_PATH ${SOURCE_PATH}/build.vc${MSVC_VERSION}/lib_mpir_gc/lib_mpir_gc.vcxproj
+		TARGET_PLATFORM_VERSION ${WINDOWS_SDK}
+	)
+endif()
+
+IF (TRIPLET_SYSTEM_ARCH MATCHES "x86")
+	SET(BUILD_ARCH "Win32")
+ELSE()
+	SET(BUILD_ARCH ${TRIPLET_SYSTEM_ARCH})
+ENDIF()
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+	file(INSTALL
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Debug/gmp.h
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Debug/gmpxx.h
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Debug/mpir.h
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Debug/mpirxx.h
+		DESTINATION ${CURRENT_PACKAGES_DIR}/include
+	)
+	file(INSTALL
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Debug/mpir.dll
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Debug/mpir.pdb
+		DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin
+	)
+	file(INSTALL
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Release/mpir.dll
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Release/mpir.pdb
+		DESTINATION ${CURRENT_PACKAGES_DIR}/bin
+	)
+	file(INSTALL
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Debug/mpir.lib
+		DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+	)
+	file(INSTALL
+		${SOURCE_PATH}/dll/${BUILD_ARCH}/Release/mpir.lib
+		DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+	)
+	file(INSTALL ${SOURCE_PATH}/COPYING.lib DESTINATION ${CURRENT_PACKAGES_DIR}/share/mpir RENAME copyright)
+	vcpkg_copy_pdbs()
+else()
+	file(INSTALL
+		${SOURCE_PATH}/lib/${BUILD_ARCH}/Debug/gmp.h
+		${SOURCE_PATH}/lib/${BUILD_ARCH}/Debug/gmpxx.h
+		${SOURCE_PATH}/lib/${BUILD_ARCH}/Debug/mpir.h
+		${SOURCE_PATH}/lib/${BUILD_ARCH}/Debug/mpirxx.h
+		DESTINATION ${CURRENT_PACKAGES_DIR}/include
+	)
+	file(INSTALL
+		${SOURCE_PATH}/lib/${BUILD_ARCH}/Debug/mpir.lib
+		DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib
+	)
+	file(INSTALL
+		${SOURCE_PATH}/lib/${BUILD_ARCH}/Release/mpir.lib
+		DESTINATION ${CURRENT_PACKAGES_DIR}/lib
+	)
+	file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/mpir RENAME copyright)
+endif()
+
+message(STATUS "Installing done")
