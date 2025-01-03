2ddf82ca301347fff9882d818ce162f7cba10097
Capric <capric8416@gmail.com>
2025-01-03 18:21:33 +0800

    flatten https://github.com/microsoft/vcpkg/commit/d67ae2f from arkady.shapkin@gmail.com at 2017-07-08 03:42:54 +0300

---
+++ ports/grpc-1-4-1-0/portfile.cmake
@@ -0,0 +1,71 @@
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    message(STATUS "Warning: Dynamic building not supported yet. Building static.")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+include(vcpkg_common_functions)
+
+if(EXISTS "${CURRENT_BUILDTREES_DIR}/src/.git")
+    file(REMOVE_RECURSE ${CURRENT_BUILDTREES_DIR}/src)
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO grpc/grpc
+    REF v1.4.1
+    SHA512 5028e4f881a41e4c4ddf770bd824d1d5de825f86d68dbbfab22e2a34ec0e46b27754b0f5b40cfc02b0d22a756b08056b100837b590745b5fdbdce9a803e59f8d
+    HEAD_REF master
+)
+
+# Issue: https://github.com/grpc/grpc/issues/10759
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES
+        ${CMAKE_CURRENT_LIST_DIR}/revert-c019e05.patch
+        ${CMAKE_CURRENT_LIST_DIR}/disable-csharp-ext.patch
+)
+
+if(VCPKG_CRT_LINKAGE STREQUAL static)
+    set(gRPC_MSVC_STATIC_RUNTIME ON)
+else()
+    set(gRPC_MSVC_STATIC_RUNTIME OFF)
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DgRPC_INSTALL=ON
+        -DgRPC_BUILD_TESTS=OFF
+        -DgRPC_MSVC_STATIC_RUNTIME=${gRPC_MSVC_STATIC_RUNTIME}
+        -DgRPC_ZLIB_PROVIDER=package
+        -DgRPC_SSL_PROVIDER=package
+        -DgRPC_PROTOBUF_PROVIDER=package
+        -DgRPC_CARES_PROVIDER=package
+        -DgRPC_GFLAGS_PROVIDER=none
+        -DgRPC_BENCHMARK_PROVIDER=none
+        -DgRPC_INSTALL_CSHARP_EXT=OFF
+        -DCMAKE_INSTALL_CMAKEDIR=share/grpc
+)
+
+# gRPC runs built executables during the build, so they need access to the installed DLLs.
+set(ENV{PATH} "$ENV{PATH};${CURRENT_INSTALLED_DIR}/bin;${CURRENT_INSTALLED_DIR}/debug/bin")
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/grpc RENAME copyright)
+
+# Install tools and plugins
+file(GLOB TOOLS "${CURRENT_BUILDTREES_DIR}/${TARGET_TRIPLET}-rel/*.exe")
+if(TOOLS)
+    file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/tools/grpc)
+    file(COPY ${TOOLS} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/grpc)
+    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/grpc)
+endif()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_copy_pdbs()
