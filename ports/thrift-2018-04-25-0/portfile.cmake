8f92d10887d4cbf976608d6b5e8cf920e575a454
Capric <capric8416@gmail.com>
2025-01-04 00:16:25 +0800

    flatten https://github.com/microsoft/vcpkg/commit/8eac561 from roschuma@microsoft.com at 2018-05-01 15:49:11 -0700

---
+++ ports/thrift-2018-04-25-0/portfile.cmake
@@ -0,0 +1,55 @@
+include(vcpkg_common_functions)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    message(STATUS "Warning: Dynamic building not supported. Building static.") # See note below
+    set(VCPKG_LIBRARY_LINKAGE static)
+
+    # As per Ben Craig thrift comment see https://issues.apache.org/jira/browse/THRIFT-1834
+    # Currently, Thrift is designed to be packaged as a static library. As a static library, the consuming program / dll will only pull in the object files that it needs, so the per-binary size increase should be pretty small.
+    # Thrift isn't a very good candidate to become a dynamic library. No attempts are made to preserve binary compatibility, or to provide a C / COM-like interface to make binary compatibility easy.
+endif()
+
+vcpkg_find_acquire_program(FLEX)
+vcpkg_find_acquire_program(BISON)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO apache/thrift
+    REF 0293c3643b0075b510b9d92b2cc39b6d5e0fadb8
+    SHA512 2bf217d9317b35215d5f984ae76509ddf505459dec97fde6aae8b4a2632882df971c438a99afc87fbe28dd931e2d1283315d1dbb3eb8af92c94fa0fdffd05ac0
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DWITH_SHARED_LIB=OFF
+        -DWITH_STATIC_LIB=ON
+        -DWITH_STDTHREADS=ON
+        -DBUILD_TESTING=off
+        -DBUILD_JAVA=off
+        -DBUILD_C_GLIB=off
+        -DBUILD_PYTHON=off
+        -DBUILD_CPP=on
+        -DBUILD_HASKELL=off
+        -DBUILD_TUTORIALS=off
+        -DFLEX_EXECUTABLE=${FLEX}
+        -DBISON_EXECUTABLE=${BISON}
+)
+
+vcpkg_install_cmake()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/thrift RENAME copyright)
+
+file(GLOB EXES "${CURRENT_PACKAGES_DIR}/bin/*.exe")
+if(EXES)
+    file(COPY ${EXES} DESTINATION ${CURRENT_PACKAGES_DIR}/tools/thrift)
+    file(REMOVE ${EXES})
+    vcpkg_copy_tool_dependencies(${CURRENT_PACKAGES_DIR}/tools/thrift)
+endif()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+vcpkg_copy_pdbs()
