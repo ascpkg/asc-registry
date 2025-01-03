49e6f92dfab4b84bc9b9551194c64a216edc2e68
Capric <capric8416@gmail.com>
2025-01-03 16:34:46 +0800

    flatten https://github.com/microsoft/vcpkg/commit/2538bda from sdflysha@qq.com at 2016-11-21 11:19:08 +0800

---
+++ ports/libbson-1-5-0-rc6-0/portfile.cmake
@@ -0,0 +1,38 @@
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/libbson-1.5.0-rc6)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/mongodb/libbson/archive/1.5.0-rc6.tar.gz"
+    FILENAME "libbson-1.5.0-rc6.tar.gz"
+    SHA512 15cf590b488f7de8d614ddcc2c1536b05b607311f3cd3353418469c7a62177124fb4fb1c53f51b0de4c7491b21051c1ec47fbc12856cc69e37baebb3d65897c5
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+	OPTIONS
+		-DENABLE_TESTS=OFF
+		-DCMAKE_INSTALL_SYSTEM_RUNTIME_LIBS_SKIP=ON
+)
+
+vcpkg_install_cmake()
+
+file(RENAME
+	${CURRENT_PACKAGES_DIR}/include/libbson-1.0
+	${CURRENT_PACKAGES_DIR}/temp)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include)
+file(RENAME ${CURRENT_PACKAGES_DIR}/temp ${CURRENT_PACKAGES_DIR}/include)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL static)
+	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+	file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin)
+	file(REMOVE         ${CURRENT_PACKAGES_DIR}/lib/bson-1.0.lib)
+	file(REMOVE         ${CURRENT_PACKAGES_DIR}/debug/lib/bson-1.0.lib)
+else()
+	file(REMOVE         ${CURRENT_PACKAGES_DIR}/lib/bson-static-1.0.lib)
+	file(REMOVE         ${CURRENT_PACKAGES_DIR}/debug/lib/bson-static-1.0.lib)
+endif()
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/libbson RENAME copyright)
