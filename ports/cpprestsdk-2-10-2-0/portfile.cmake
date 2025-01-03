f19299c02df18b0046bb074786d7e3707c871dd7
Capric <capric8416@gmail.com>
2025-01-03 21:54:13 +0800

    flatten https://github.com/microsoft/vcpkg/commit/3d0ace3 from roschuma@microsoft.com at 2018-02-06 15:45:13 -0800

---
+++ ports/cpprestsdk-2-10-2-0/portfile.cmake
@@ -0,0 +1,42 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Microsoft/cpprestsdk
+    REF v2.10.2
+    SHA512 267a928f770a668874f9b7e381e8977ffa3478c9292df4ed93d4235d20f0e89b1bfe4cfc82945d3f28fe2746a4fe6089009c4839dac5db130494bb3a395dd198
+    HEAD_REF master
+)
+
+set(OPTIONS)
+if(NOT VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    SET(WEBSOCKETPP_PATH "${CURRENT_INSTALLED_DIR}/share/websocketpp")
+    list(APPEND OPTIONS
+        -DWEBSOCKETPP_CONFIG=${WEBSOCKETPP_PATH}
+        -DWEBSOCKETPP_CONFIG_VERSION=${WEBSOCKETPP_PATH})
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}/Release
+    PREFER_NINJA
+    OPTIONS
+        ${OPTIONS}
+        -DBUILD_TESTS=OFF
+        -DBUILD_SAMPLES=OFF
+        -DCPPREST_EXCLUDE_WEBSOCKETS=OFF
+        -DCPPREST_EXPORT_DIR=share/cpprestsdk
+        -DWERROR=OFF
+    OPTIONS_DEBUG
+        -DCPPREST_INSTALL_HEADERS=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets()
+
+file(INSTALL
+    ${SOURCE_PATH}/license.txt
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/cpprestsdk RENAME copyright)
+
+vcpkg_copy_pdbs()
+
