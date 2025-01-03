9253b2d1dad899c73fa9d98ae50682a753f504fb
Capric <capric8416@gmail.com>
2025-01-03 23:53:22 +0800

    flatten https://github.com/microsoft/vcpkg/commit/0764aaf from roschuma@microsoft.com at 2018-04-12 04:03:17 -0700

---
+++ ports/abseil-2018-04-12-0/portfile.cmake
@@ -0,0 +1,36 @@
+include(vcpkg_common_functions)
+
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    message(FATAL_ERROR "Abseil currently only supports being built for desktop")
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO abseil/abseil-cpp
+    REF a7e522daf1ec9cda69b356472f662142dd0c1215
+    SHA512 250b602fc3f5a4c3b6522ef0f49bdf63db0070289cb415cb31bb672b95b35dc9494f881adeb1b090927b1cd942c3674b6142747f601c8f7233bd12d4078583f3
+    HEAD_REF master
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-abseil TARGET_PATH share/unofficial-abseil)
+
+file(GLOB_RECURSE HEADERS ${CURRENT_PACKAGES_DIR}/include/*)
+foreach(FILE ${HEADERS})
+    file(READ "${FILE}" _contents)
+    string(REPLACE "std::min(" "(std::min)(" _contents "${_contents}")
+    string(REPLACE "std::max(" "(std::max)(" _contents "${_contents}")
+    file(WRITE "${FILE}" "${_contents}")
+endforeach()
+
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/abseil RENAME copyright)
