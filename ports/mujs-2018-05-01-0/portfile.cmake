f054d8aaf218b372db12dac10fd4733594c21b1f
Capric <capric8416@gmail.com>
2025-01-04 00:27:52 +0800

    flatten https://github.com/microsoft/vcpkg/commit/fcb3033 from roschuma@microsoft.com at 2018-05-01 15:55:59 -0700

---
+++ ports/mujs-2018-05-01-0/portfile.cmake
@@ -0,0 +1,27 @@
+include(vcpkg_common_functions)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+  message(WARNING "building static")
+  set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO ccxvii/mujs
+  REF 780b14b5b73724c36800bf72db1f8f1a2b77f76d
+  SHA512 e320d36794811136a3a9ea2c750736694368007873332682491baa38472100db918f5f237ce6c351498c0d83ffce7185c6b86a87223a28eb511b12ea086aa4aa
+  HEAD_REF master
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+  SOURCE_PATH ${SOURCE_PATH}
+  PREFER_NINJA
+  OPTIONS_DEBUG -DDISABLE_INSTALL_HEADERS=ON -DDISABLE_INSTALL_TOOLS=ON
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/mujs RENAME copyright)
