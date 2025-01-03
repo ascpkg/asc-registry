db5a3630f90db41a8625fe078bf2069bf926b749
Capric <capric8416@gmail.com>
2025-01-03 17:31:03 +0800

    flatten https://github.com/microsoft/vcpkg/commit/61a5a8a from rob.ceglinski@gmail.com at 2017-05-04 17:47:22 +0200

---
+++ ports/pango-1-40-5-2-0/portfile.cmake
@@ -0,0 +1,28 @@
+include(vcpkg_common_functions)
+set(PANGO_VERSION 1.40.5)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/pango-${PANGO_VERSION})
+vcpkg_download_distfile(ARCHIVE
+    URLS "http://ftp.gnome.org/pub/GNOME/sources/pango/1.40/pango-${PANGO_VERSION}.tar.xz"
+    FILENAME "pango-${PANGO_VERSION}.tar.xz"
+    SHA512 40e8bf85dbb4b6fd35da3acec06a0d2b9dde95a3c5a212d243dbcbc0d00f12bd061757a04cb2f4a8db61329efd7ed9be53e3f5d6a2eb2a3defba1d12f9eed43d)
+
+vcpkg_extract_source_archive(${ARCHIVE})
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES
+        ${CMAKE_CURRENT_LIST_DIR}/0001-fix-static-symbols-export.diff)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+	OPTIONS_DEBUG
+	    -DPANGO_SKIP_HEADERS=ON)
+
+vcpkg_build_cmake()
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/pango)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/pango/COPYING ${CURRENT_PACKAGES_DIR}/share/pango/copyright)
