2b7c55b5f4e2c66701996c1e244821b0fc094813
Capric <capric8416@gmail.com>
2025-01-03 16:21:54 +0800

    flatten https://github.com/microsoft/vcpkg/commit/8d1d8df from cbond01@gmail.com at 2016-10-16 17:18:11 -0700

---
+++ ports/sqlite3-3-15-0-3150000-0/portfile.cmake
@@ -0,0 +1,19 @@
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CMAKE_CURRENT_LIST_DIR})
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://sqlite.org/2016/sqlite-amalgamation-3150000.zip"
+    FILENAME "sqlite-amalgamation-3150000.zip"
+    SHA512 82fea23b2158c448cbe2b80121eb32652df49eb85357edbaeef0c343ef478433706ebc4cd8add1985763db223d9268d0f7e74fc8db59353c15267cbc3d2078a8
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DSOURCE=${CURRENT_BUILDTREES_DIR}/src/sqlite-amalgamation-3150000
+)
+
+vcpkg_install_cmake()
+
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/sqlite3/copyright "SQLite is in the Public Domain.\nhttp://www.sqlite.org/copyright.html\n")
+vcpkg_copy_pdbs()
