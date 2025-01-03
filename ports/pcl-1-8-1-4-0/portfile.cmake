924b06460b0334d5044c0fbf548bca2e34d517d4
Capric <capric8416@gmail.com>
2025-01-03 20:13:27 +0800

    flatten https://github.com/microsoft/vcpkg/commit/f625230 from roschuma@microsoft.com at 2017-11-10 00:31:46 -0800

---
+++ ports/pcl-1-8-1-4-0/portfile.cmake
@@ -0,0 +1,62 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO PointCloudLibrary/pcl
+    REF pcl-1.8.1
+    SHA512 9e7c87fb750a176712f08d215a906012c9e8174b687bbc8c08fa65de083b4468951bd8017b10409015d5eff0fc343885d2aae5c340346118b1a251af7bdd5cd7
+    HEAD_REF master
+)
+
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/config.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/config_install.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/find_flann.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/find_qhull.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/find_openni2.patch"
+            "${CMAKE_CURRENT_LIST_DIR}/vs2017-15.4-workaround.patch"
+)
+
+string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" PCL_SHARED_LIBS)
+
+set(WITH_QT OFF)
+if("qt" IN_LIST FEATURES)
+    set(WITH_QT ON)
+endif()
+
+set(WITH_PCAP OFF)
+if("pcap" IN_LIST FEATURES)
+    set(WITH_PCAP ON)
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        # BUILD
+        -DBUILD_surface_on_nurbs=ON
+        -DBUILD_tools=OFF
+        # PCL
+        -DPCL_BUILD_WITH_BOOST_DYNAMIC_LINKING_WIN32=${PCL_SHARED_LIBS}
+        -DPCL_BUILD_WITH_FLANN_DYNAMIC_LINKING_WIN32=${PCL_SHARED_LIBS}
+        -DPCL_SHARED_LIBS=${PCL_SHARED_LIBS}
+        # WITH
+        -DWITH_CUDA=OFF
+        -DWITH_LIBUSB=OFF
+        -DWITH_OPENNI2=ON
+        -DWITH_PCAP=${WITH_PCAP}
+        -DWITH_PNG=OFF
+        -DWITH_QHULL=ON
+        -DWITH_QT=${WITH_QT}
+        -DWITH_VTK=ON
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/pcl)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/pcl)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/pcl/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/pcl/copyright)
