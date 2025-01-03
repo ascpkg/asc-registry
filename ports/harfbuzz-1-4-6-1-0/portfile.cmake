d8be4efa067626288a853bbf2eb20c3de621cec3
Capric <capric8416@gmail.com>
2025-01-03 17:50:08 +0800

    flatten https://github.com/microsoft/vcpkg/commit/dff3a89 from roschuma@microsoft.com at 2017-06-05 16:58:06 -0700

---
+++ ports/harfbuzz-1-4-6-1-0/portfile.cmake
@@ -0,0 +1,39 @@
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/harfbuzz-1.4.6)
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/behdad/harfbuzz/releases/download/1.4.6/harfbuzz-1.4.6.tar.bz2"
+    FILENAME "harfbuzz-1.4.6.tar.bz2"
+    SHA512 aade3902adadf3a8339ba1d05279e639da7cb53981adc64e2a2d32a5d49335a6a9782a62cdf80beca569ec8a639792bf0368c0b6ecad08f35bc85878678aa096
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_apply_patches(
+    SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/0001-fix-uwp-build.patch"
+)
+
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "arm")
+    SET(HAVE_GLIB "OFF")
+	SET(BUILTIN_UCDN "ON")
+else()
+    SET(HAVE_GLIB "ON")
+	SET(BUILTIN_UCDN "OFF")
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DHB_HAVE_FREETYPE=ON
+        -DHB_HAVE_GLIB=${HAVE_GLIB}
+        -DHB_BUILTIN_UCDN=${BUILTIN_UCDN}
+    OPTIONS_DEBUG
+        -DSKIP_INSTALL_HEADERS=ON
+)
+
+vcpkg_install_cmake()
+vcpkg_copy_pdbs()
+
+# Handle copyright
+file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/harfbuzz)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/harfbuzz/COPYING ${CURRENT_PACKAGES_DIR}/share/harfbuzz/copyright)
