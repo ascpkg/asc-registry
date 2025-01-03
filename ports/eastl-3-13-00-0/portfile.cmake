b7e5248c37df6a8508c94e0049852844ca3c91ce
Capric <capric8416@gmail.com>
2025-01-04 06:07:46 +0800

    flatten https://github.com/microsoft/vcpkg/commit/1207951 from roschuma@microsoft.com at 2019-01-18 14:40:03 -0800

---
+++ ports/eastl-3-13-00-0/portfile.cmake
@@ -0,0 +1,29 @@
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    message(STATUS "Warning: Dynamic building not supported by EASTL. Building static.")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/eastl)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO electronicarts/EASTL
+    REF 3.13.00
+    SHA512 5a0a0491e98c1ef18d979865c02f46b3252ecf2757d2f9c5bb07181dfb7b45ff1bba900640fce3771aa4ac06a3e432ce514d1d358ff2066261d0197385419ddb
+    HEAD_REF master
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_copy_pdbs()
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/eastl RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/3RDPARTYLICENSES.TXT DESTINATION ${CURRENT_PACKAGES_DIR}/share/eastl)
