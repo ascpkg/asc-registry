68d9d67e39f8bef2ccb51c62b7bed5dbc1011866
Capric <capric8416@gmail.com>
2025-01-03 18:51:31 +0800

    flatten https://github.com/microsoft/vcpkg/commit/3115c1d from convery10122@gmail.com at 2017-08-11 02:54:37 +0200

---
+++ ports/uwebsockets-0-14-4-0/portfile.cmake
@@ -0,0 +1,25 @@
+include(vcpkg_common_functions)
+set(VERSION 0.14.3)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/uwebsockets-${VERSION})
+vcpkg_download_distfile(ARCHIVE
+    URLS "https://github.com/uWebSockets/uWebSockets/archive/v${VERSION}.zip"
+    FILENAME "uwebsockets-v${VERSION}.zip"
+    SHA512 a64a6ee91d411f4285deb10b9b7620b4c4a2ff0f8137864380968349f7786c406ba44644b6ca589ee41c1e5adfa1ee1cf3697e54ecf5d459ab8bf69a3a4def7e
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS_DEBUG
+        -DINSTALL_HEADERS=OFF
+)
+
+vcpkg_install_cmake()
+
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/uwebsockets)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/uwebsockets/LICENSE ${CURRENT_PACKAGES_DIR}/share/uwebsockets/copyright)
+
+vcpkg_copy_pdbs()
