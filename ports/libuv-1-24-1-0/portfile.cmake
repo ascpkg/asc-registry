61fa93ccb7b051d597ae716aa410f64a50ef65f1
Capric <capric8416@gmail.com>
2025-01-04 05:28:09 +0800

    flatten https://github.com/microsoft/vcpkg/commit/c6db2c8 from roschuma@microsoft.com at 2019-01-09 12:31:28 -0800

---
+++ ports/libuv-1-24-1-0/portfile.cmake
@@ -0,0 +1,39 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO libuv/libuv
+    REF v1.24.1
+    SHA512 19cb03a7a62ad2a1f392e1de3e4fdb622e3ac5afe890bfb819c30118f4745f81ecc343d5b633eebb8471fdfb4025652bbface245fd616c55346aae82376f6836
+    HEAD_REF v1.x
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS_DEBUG
+        -DUV_SKIP_HEADERS=ON
+)
+
+vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-libuv TARGET_PATH share/unofficial-libuv)
+vcpkg_copy_pdbs()
+
+configure_file(
+    ${CMAKE_CURRENT_LIST_DIR}/unofficial-libuv-config.in.cmake
+    ${CURRENT_PACKAGES_DIR}/share/unofficial-libuv/unofficial-libuv-config.cmake
+    @ONLY
+)
+
+file(READ ${CURRENT_PACKAGES_DIR}/include/uv.h UV_H)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "dynamic")
+    string(REPLACE "defined(USING_UV_SHARED)" "1" UV_H "${UV_H}")
+else()
+    string(REPLACE "defined(USING_UV_SHARED)" "0" UV_H "${UV_H}")
+endif()
+file(WRITE ${CURRENT_PACKAGES_DIR}/include/uv.h "${UV_H}")
+
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/libuv)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/libuv/LICENSE ${CURRENT_PACKAGES_DIR}/share/libuv/copyright)
