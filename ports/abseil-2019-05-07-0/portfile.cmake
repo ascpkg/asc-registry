0c46e32b72091773e0b3666db01cc554b76da6b3
Capric <capric8416@gmail.com>
2025-01-04 10:14:20 +0800

    flatten https://github.com/microsoft/vcpkg/commit/223d5d8 from roschuma@microsoft.com at 2019-05-08 17:51:44 -0700

---
+++ ports/abseil-2019-05-07-0/portfile.cmake
@@ -0,0 +1,31 @@
+include(vcpkg_common_functions)
+
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    message(FATAL_ERROR "Abseil currently only supports being built for desktop")
+endif()
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO abseil/abseil-cpp
+    REF cd86d0d20ab167c33b23d3875db68d1d4bad3a3b
+    SHA512 88e3edf395a11bdcd09d831dee3fad14a556761aafbc207e09ce17865e0663d9039a7d7954c95576270f7207cd00176b2ea107c61c6a059a5627c6fe062a66b5
+    HEAD_REF master
+    PATCHES fix-usage-lnk-error.patch
+)
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH share/unofficial-abseil TARGET_PATH share/unofficial-abseil)
+
+vcpkg_copy_pdbs()
+
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/abseil RENAME copyright)
