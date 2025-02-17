1be2be5727b2a2e869dd30f4069c025c3863bef1
Capric <capric8416@gmail.com>
2025-02-17 17:25:43 +0800

    flatten https://github.com/microsoft/vcpkg/commit/938a8fe40 from barathsotd@gmail.com at 2017-11-05 19:44:44 +1100

--- ports/qt5multimedia-5-9-2-0-0/portfile.cmake
+++ ports/qt5multimedia-5-9-2-0-0/portfile.cmake
@@ -13,15 +13,15 @@ endif()
 
 list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
-set(SRCDIR_NAME "qtspeech-5.9.2")
-set(ARCHIVE_NAME "qtspeech-opensource-src-5.9.2")
+set(SRCDIR_NAME "qtmultimedia-5.9.2")
+set(ARCHIVE_NAME "qtmultimedia-opensource-src-5.9.2")
 set(ARCHIVE_EXTENSION ".tar.xz")
 
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${SRCDIR_NAME})
 vcpkg_download_distfile(ARCHIVE_FILE
     URLS "http://download.qt.io/official_releases/qt/5.9/5.9.2/submodules/${ARCHIVE_NAME}${ARCHIVE_EXTENSION}"
     FILENAME ${SRCDIR_NAME}${ARCHIVE_EXTENSION}
-    SHA512 8213ad13d33732fee3fc5b5e408b870970a3003d461be24195222e1d1209210039d1d0cd2f3c537a0db62629fb297d88b33ed4734ecb6c8d74f5406a87e7e0c0
+    SHA512 b9fab874706440e97185475bfd0ad769c23d5ddbff5086cc0da9783777e81ed8140fb06fa0e7536ebfb67f2c9db39e1b9f3c2241834e74a9e1fb6ffd5cb7af11
 )
 vcpkg_extract_source_archive(${ARCHIVE_FILE})
 if (EXISTS ${CURRENT_BUILDTREES_DIR}/src/${ARCHIVE_NAME})
@@ -78,4 +78,4 @@ file(GLOB RELEASE_DLLS "${CURRENT_PACKAGES_DIR}/lib/*.dll")
 file(GLOB DEBUG_DLLS "${CURRENT_PACKAGES_DIR}/debug/lib/*.dll")
 file(REMOVE ${RELEASE_DLLS} ${DEBUG_DLLS})
 
-file(INSTALL ${SOURCE_PATH}/LICENSE.LGPLv3 DESTINATION ${CURRENT_PACKAGES_DIR}/share/qt5speech RENAME copyright)
+file(INSTALL ${SOURCE_PATH}/LICENSE.LGPL3 DESTINATION ${CURRENT_PACKAGES_DIR}/share/qt5multimedia RENAME copyright)
