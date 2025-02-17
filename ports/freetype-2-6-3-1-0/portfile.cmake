5deb3c3780a33b9a228bfb41ccde662208d2d52e
Capric <capric8416@gmail.com>
2025-02-17 17:19:37 +0800

    flatten https://github.com/microsoft/vcpkg/commit/c494892eb from roschuma@microsoft.com at 2016-10-10 23:07:25 -0700

---
+++ ports/freetype-2-6-3-1-0/portfile.cmake
@@ -0,0 +1,44 @@
+include(vcpkg_common_functions)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/freetype-2.6.3)
+vcpkg_download_distfile(ARCHIVE
+    URLS "http://download.savannah.gnu.org/releases/freetype/freetype-2.6.3.tar.bz2"
+    FILENAME "freetype-2.6.3.tar.bz2"
+    SHA512 e1f9018835fc88beeb4479537b59f866c52393ae18d24a1e0710a464cf948ab02b35c2c6043bc20c1db3a04871ee4eb0bb1d210550c0ea2780c8b1aea98fbf0d
+)
+vcpkg_extract_source_archive(${ARCHIVE})
+
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES ${CMAKE_CURRENT_LIST_DIR}/0001-Support-Windows-DLLs-via-CMAKE_WINDOWS_EXPORT_ALL_SY.patch
+            ${CMAKE_CURRENT_LIST_DIR}/0002-Add-CONFIG_INSTALL_PATH-option.patch
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    OPTIONS
+        -DBUILD_SHARED_LIBS=ON
+        -DCONFIG_INSTALL_PATH=share/freetype
+)
+
+vcpkg_install_cmake()
+
+file(RENAME ${CURRENT_PACKAGES_DIR}/include/freetype2/freetype ${CURRENT_PACKAGES_DIR}/include/freetype)
+file(RENAME ${CURRENT_PACKAGES_DIR}/include/freetype2/ft2build.h ${CURRENT_PACKAGES_DIR}/include/ft2build.h)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/freetype2)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+file(READ ${CURRENT_PACKAGES_DIR}/debug/share/freetype/freetype-config-debug.cmake DEBUG_MODULE)
+string(REPLACE "\${_IMPORT_PREFIX}" "\${_IMPORT_PREFIX}/debug" DEBUG_MODULE "${DEBUG_MODULE}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/share/freetype/freetype-config-debug.cmake "${DEBUG_MODULE}")
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
+
+file(COPY
+    ${SOURCE_PATH}/docs/LICENSE.TXT
+    ${SOURCE_PATH}/docs/FTL.TXT
+    ${SOURCE_PATH}/docs/GPLv2.TXT
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/freetype
+)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/freetype/LICENSE.TXT ${CURRENT_PACKAGES_DIR}/share/freetype/copyright)
+vcpkg_copy_pdbs()
+
