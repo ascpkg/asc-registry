74dce41ddf5ebc326f0d6c06de19bba57302dcd4
Capric <capric8416@gmail.com>
2025-02-17 17:24:57 +0800

    flatten https://github.com/microsoft/vcpkg/commit/e30466b08 from ehysta@gmail.com at 2017-10-04 07:12:43 +0300

---
+++ ports/sciter-4-0-4-0/portfile.cmake
@@ -0,0 +1,74 @@
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    message(STATUS "Warning: Sciter is only available under a free license as DLLs.")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif()
+
+if(VCPKG_CMAKE_SYSTEM_NAME STREQUAL "WindowsStore")
+    message(FATAL_ERROR "Sciter only supports Windows Desktop")
+endif()
+
+include(vcpkg_common_functions)
+
+# header-only library
+set(VCPKG_POLICY_DLLS_WITHOUT_LIBS enabled)
+
+set(SCITER_VERSION 4.0.4)
+set(SCITER_REVISION 172e04dc1b8527a6f7ea8b6e9388238f9568cc93)
+set(SCITER_SHA 07db84fe560450b32477a35fd63d6d9ba5a5df1cc4bccba6e0f74cde56f16a2708f58b7f27482a97e4264e7069096128a591c886f19e56fc2fd8d16ddcbcf73e)
+
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL x64)
+    set(SCITER_ARCH 64)
+elseif(VCPKG_TARGET_ARCHITECTURE STREQUAL x86)
+    set(SCITER_ARCH 32)
+endif()
+
+# check out
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO c-smile/sciter-sdk
+    REF ${SCITER_REVISION}
+    SHA512 ${SCITER_SHA}
+)
+
+# disable stdafx.h
+vcpkg_apply_patches(
+    SOURCE_PATH ${SOURCE_PATH}
+    PATCHES ${CMAKE_CURRENT_LIST_DIR}/0001_patch_stdafx.patch
+)
+
+# install include directory
+file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include/sciter
+    FILES_MATCHING
+    PATTERN "sciter-gtk-main.cpp" EXCLUDE
+    PATTERN "sciter-osx-main.mm" EXCLUDE
+    PATTERN "*.cpp"
+    PATTERN "*.h"
+    PATTERN "*.hpp"
+)
+
+set(SCITER_SHARE ${CURRENT_PACKAGES_DIR}/share/sciter)
+set(SCITER_TOOLS ${CURRENT_PACKAGES_DIR}/tools/sciter)
+
+# license
+file(COPY ${SOURCE_PATH}/logfile.htm DESTINATION ${SCITER_SHARE})
+file(COPY ${SOURCE_PATH}/license.htm DESTINATION ${SCITER_SHARE})
+file(RENAME ${SCITER_SHARE}/license.htm ${SCITER_SHARE}/copyright)
+
+# samples & widgets
+file(COPY ${SOURCE_PATH}/samples DESTINATION ${SCITER_SHARE})
+file(COPY ${SOURCE_PATH}/widgets DESTINATION ${SCITER_SHARE})
+
+# tools
+file(INSTALL ${SOURCE_PATH}/bin/packfolder.exe DESTINATION ${SCITER_TOOLS})
+file(INSTALL ${SOURCE_PATH}/bin/tiscript.exe DESTINATION ${SCITER_TOOLS})
+
+file(INSTALL ${SOURCE_PATH}/bin/${SCITER_ARCH}/sciter.exe DESTINATION ${SCITER_TOOLS})
+file(INSTALL ${SOURCE_PATH}/bin/${SCITER_ARCH}/inspector.exe DESTINATION ${SCITER_TOOLS})
+file(INSTALL ${SOURCE_PATH}/bin/${SCITER_ARCH}/sciter.dll DESTINATION ${SCITER_TOOLS})
+
+file(INSTALL ${SOURCE_PATH}/bin/${SCITER_ARCH}/sciter.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+file(INSTALL ${SOURCE_PATH}/bin/${SCITER_ARCH}/sciter.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+file(INSTALL ${SOURCE_PATH}/bin/${SCITER_ARCH}/tiscript-sqlite.dll DESTINATION ${CURRENT_PACKAGES_DIR}/bin)
+file(INSTALL ${SOURCE_PATH}/bin/${SCITER_ARCH}/tiscript-sqlite.dll DESTINATION ${CURRENT_PACKAGES_DIR}/debug/bin)
+
+message(STATUS "Warning: Sciter requires manual deployment of the correct DLL files.")
