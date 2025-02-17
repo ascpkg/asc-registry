a7ec425de8f3dc9fa08e4ac7602ea9631475c1e5
Capric <capric8416@gmail.com>
2025-02-17 17:20:17 +0800

    flatten https://github.com/microsoft/vcpkg/commit/420795eed from kirk.shoop@microsoft.com at 2016-11-25 19:27:07 -0800

--- ports/rxcpp-3-0-0-0/portfile.cmake
+++ ports/rxcpp-3-0-0-0/portfile.cmake
@@ -1,20 +1,15 @@
 #header-only library
 include(vcpkg_common_functions)
 set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/RxCpp-3.0.0)
-vcpkg_download_distfile(ARCHIVE
+vcpkg_download_distfile(ARCHIVE_FILE
     URLS "https://github.com/Reactive-Extensions/RxCpp/archive/v3.0.0.tar.gz"
-    FILENAME "v3.0.0.tar.gz"
-    SHA512 6d810b6163d0920d531f32a13729e290c81b47d5fc9c3e3d3d8a25d27a6f0671fec097d091bef7383b7e556e9e5471db087bb955e7f4fd9a5fdc9e7b06050844
+    FILENAME "RxCpp-3.0.0.tar.gz"
+    SHA512 f30f71cefee25f86297d66a49e752a44bdb8ad9a1a92249bf944101afd91b432564e9b8c9e8853f7042608030bffaa4d58d294f18a61f394701cee347f42bcbb
 )
-vcpkg_extract_source_archive(${ARCHIVE})
+vcpkg_extract_source_archive(${ARCHIVE_FILE})
 
 file(INSTALL
-	${SOURCE_PATH}/Rx/v2/src/rxcpp
-    DESTINATION ${CURRENT_PACKAGES_DIR}/include
-)
-
-file(INSTALL
-	${SOURCE_PATH}/Ix/CPP/src/cpplinq
+	${SOURCE_PATH}/Rx/v2/src
     DESTINATION ${CURRENT_PACKAGES_DIR}/include
 )
 
