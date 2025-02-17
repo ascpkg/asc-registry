47ae0556180e00e3695b61baa79cee9f340144fd
Capric <capric8416@gmail.com>
2025-02-17 20:41:49 +0800

    flatten https://github.com/microsoft/vcpkg/commit/5bb5f3923 from dg0yt@darc.de at 2023-01-09 23:15:51 +0100

---
+++ ports/pango-1-50-12-1/portfile.cmake
@@ -0,0 +1,35 @@
+vcpkg_minimum_required(VERSION 2022-10-12) # for ${VERSION}
+vcpkg_from_gitlab(
+    GITLAB_URL https://gitlab.gnome.org/
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO GNOME/pango
+    REF "${VERSION}"
+    SHA512 30be64784fac2539741c5e085ed0cf515fdd2044e71b6c08121dfb838e50d1a31f4f9e53d9869b65aa1fa246ab5cc1bec18503a08dcf0337aece84290472e336
+    HEAD_REF master
+    PATCHES
+        freetype2-pc.patch
+) 
+
+vcpkg_configure_meson(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -Dintrospection=disabled # Build the GObject introspection data for Pango
+        -Dfontconfig=enabled # Build with FontConfig support.
+        -Dsysprof=disabled # include tracing support for sysprof
+        -Dlibthai=disabled # Build with libthai support
+        -Dcairo=enabled # Build with cairo support
+        -Dxft=disabled # Build with xft support
+        -Dfreetype=enabled # Build with freetype support
+        -Dgtk_doc=false #Build API reference for Pango using GTK-Doc
+    ADDITIONAL_BINARIES
+        "glib-genmarshal='${CURRENT_HOST_INSTALLED_DIR}/tools/glib/glib-genmarshal'"
+        "glib-mkenums='${CURRENT_HOST_INSTALLED_DIR}/tools/glib/glib-mkenums'"
+)
+
+vcpkg_install_meson()
+vcpkg_fixup_pkgconfig()
+vcpkg_copy_pdbs()
+
+vcpkg_copy_tools(TOOL_NAMES pango-view pango-list pango-segmentation AUTO_CLEAN)
+
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
