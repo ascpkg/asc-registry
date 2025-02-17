8d648562cec57d52ad900de507b7f55401a1de64
Capric <capric8416@gmail.com>
2025-02-17 23:38:02 +0800

    flatten https://github.com/microsoft/vcpkg/commit/aa387f3d7 from 3423893+jiayuehua@users.noreply.github.com at 2024-09-24 22:03:15 +0800

---
+++ ports/aws-c-mqtt-0-10-6-0/portfile.cmake
@@ -0,0 +1,32 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO awslabs/aws-c-mqtt
+    REF "v${VERSION}"
+    SHA512 ecb8be77edbbeac4633bd57878251f9debdf0c86654a70a2299ae9c03a8852e015b65d82a98076ea44df0b2dc3ddfead7735c2b5ff03025b2d749f873dc0f049
+    HEAD_REF master
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+         "-DCMAKE_MODULE_PATH=${CURRENT_INSTALLED_DIR}/share/aws-c-common" # use extra cmake files
+         -DBUILD_TESTING=FALSE
+)
+
+vcpkg_cmake_install()
+
+string(REPLACE "dynamic" "shared" subdir "${VCPKG_LIBRARY_LINKAGE}")
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/${PORT}/cmake/${subdir}" DO_NOT_DELETE_PARENT_CONFIG_PATH)
+vcpkg_cmake_config_fixup(CONFIG_PATH "lib/${PORT}/cmake")
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/${PORT}/${PORT}-config.cmake" [[/${type}/]] "/")
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/debug/lib/${PORT}"
+    "${CURRENT_PACKAGES_DIR}/debug/share"
+    "${CURRENT_PACKAGES_DIR}/lib/${PORT}"
+)
+
+vcpkg_copy_pdbs()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
