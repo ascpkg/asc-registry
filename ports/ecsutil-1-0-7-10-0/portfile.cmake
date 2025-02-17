a69d52b626e36c9e163969db767e8ca05246d08e
Capric <capric8416@gmail.com>
2025-02-17 18:25:53 +0800

    flatten https://github.com/microsoft/vcpkg/commit/45fa57c27 from 5549662+Jimmy-Hu@users.noreply.github.com at 2020-10-13 03:03:26 +0800

---
+++ ports/ecsutil-1-0-7-10-0/portfile.cmake
@@ -0,0 +1,47 @@
+
+vcpkg_fail_port_install(ON_ARCH "arm" ON_TARGET "UWP" "Linux" "OSX")
+if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
+    set(PLATFORM x86)
+endif()
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    set(ECSUtil_CONFIGURATION_RELEASE Release)
+    set(ECSUtil_CONFIGURATION_DEBUG Debug)
+else()
+    if (VCPKG_CRT_LINKAGE STREQUAL dynamic)
+        set(ECSUtil_CONFIGURATION_RELEASE "Release Lib")
+        set(ECSUtil_CONFIGURATION_DEBUG "Debug Lib")
+    else()
+        set(ECSUtil_CONFIGURATION_RELEASE "Release Lib Static")
+        set(ECSUtil_CONFIGURATION_DEBUG "Debug Lib Static")
+    endif()
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO EMCECS/ecs-object-client-windows-cpp
+    REF v1.0.7.10
+    SHA512 a13f39342c2468f18262968c531fa11932f0e7b1a75a11866efe9e4eabdc7ba45d638b0104da24dfab3efb61758ca50f7ecf01b2ea15c2cf39fba5c913b7784b
+    HEAD_REF master
+)
+
+vcpkg_install_msbuild(
+    SOURCE_PATH ${SOURCE_PATH}
+    PROJECT_SUBPATH ECSUtil.sln
+    PLATFORM ${PLATFORM}
+    LICENSE_SUBPATH license.txt
+    TARGET ECSUtil
+    RELEASE_CONFIGURATION ${ECSUtil_CONFIGURATION_RELEASE}
+    DEBUG_CONFIGURATION ${ECSUtil_CONFIGURATION_DEBUG}
+)
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+endif()
+
+file(COPY ${SOURCE_PATH}/ECSUtil DESTINATION ${CURRENT_PACKAGES_DIR}/include FILES_MATCHING PATTERN *.h)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/ECSUtil/res ${CURRENT_PACKAGES_DIR}/tools)
+file(REMOVE ${CURRENT_PACKAGES_DIR}/bin/NatvisAddIn.dll ${CURRENT_PACKAGES_DIR}/debug/bin/NatvisAddIn.dll)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
+endif()
