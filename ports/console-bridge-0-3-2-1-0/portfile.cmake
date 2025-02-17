a30314ef158cef3df085dbabbd112ba272ce1d10
Capric <capric8416@gmail.com>
2025-02-17 17:24:31 +0800

    flatten https://github.com/microsoft/vcpkg/commit/76f195dd8 from roschuma@microsoft.com at 2017-09-07 14:25:03 -0700

--- ports/console-bridge-0-3-2-1-0/portfile.cmake
+++ ports/console-bridge-0-3-2-1-0/portfile.cmake
@@ -17,7 +17,8 @@ vcpkg_install_cmake()
 vcpkg_copy_pdbs()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH "CMake")
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/console-bridge ${CURRENT_PACKAGES_DIR}/share/console_bridge)
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/console_bridge RENAME copyright)
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/License.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/console-bridge RENAME copyright)
