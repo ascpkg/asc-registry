ed1fd0f60c155004fec09c9191b22a1f0e5fb647
Capric <capric8416@gmail.com>
2025-02-17 17:36:59 +0800

    flatten https://github.com/microsoft/vcpkg/commit/751db7b56 from jelani.brandon@microsoft.com at 2019-01-08 15:26:06 -0800

---
+++ ports/azure-uamqp-c-1-2-11-2-0/portfile.cmake
@@ -0,0 +1,32 @@
+include(vcpkg_common_functions)
+
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Azure/azure-uamqp-c
+    REF f29401ab5eb3853390d5f573d8fb37c0c96dba16
+    SHA512 8fdee32e2a85218257ee91754873f9f8ae5e16cd2b7b10c88ab6d4115fe4378a2b08f211d8307346b0bd7688c4c896c25a4de34e9231c2506819a97bbf46dd73
+    HEAD_REF master
+)
+
+file(COPY ${CURRENT_INSTALLED_DIR}/share/azure-c-shared-utility/azure_iot_build_rules.cmake DESTINATION ${SOURCE_PATH}/deps/azure-c-shared-utility/configs/)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -Dskip_samples=ON
+        -Duse_installed_dependencies=ON
+        -Dbuild_as_dynamic=OFF
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets(CONFIG_PATH cmake TARGET_PATH share/uamqp)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
+
+configure_file(${SOURCE_PATH}/LICENSE ${CURRENT_PACKAGES_DIR}/share/azure-uamqp-c/copyright COPYONLY)
+
+vcpkg_copy_pdbs()
