0f0e5b5b54e30d2ee855206cd79a796914adbfe7
Capric <capric8416@gmail.com>
2025-02-17 17:32:43 +0800

    flatten https://github.com/microsoft/vcpkg/commit/86fecfc61 from roschuma@microsoft.com at 2018-07-08 05:15:42 -0700

---
+++ ports/directxmesh-jul2018-0/portfile.cmake
@@ -0,0 +1,50 @@
+include(vcpkg_common_functions)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+    message(STATUS "Warning: Dynamic building not supported yet. Building static.")
+    set(VCPKG_LIBRARY_LINKAGE static)
+endif()
+
+if(NOT VCPKG_CRT_LINKAGE STREQUAL "dynamic")
+  message(FATAL_ERROR "DirectXMesh only supports dynamic CRT linkage")
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO Microsoft/DirectXMesh
+    REF jul2018
+    SHA512 080306d23153c5db6d84cf1df7700d89a25405064806ca9b8d07291ebe80d89793dfe540a98313afc137964e2fb900158d40feee6bb2a68ef9af2d872f0ebe3c
+    HEAD_REF master
+)
+
+IF (TRIPLET_SYSTEM_ARCH MATCHES "x86")
+    SET(BUILD_ARCH "Win32")
+ELSE()
+    SET(BUILD_ARCH ${TRIPLET_SYSTEM_ARCH})
+ENDIF()
+
+vcpkg_build_msbuild(
+    PROJECT_PATH ${SOURCE_PATH}/DirectXMesh_Desktop_2017.sln
+    PLATFORM ${BUILD_ARCH}
+)
+
+file(INSTALL
+    ${SOURCE_PATH}/DirectXMesh/DirectXMesh.h
+    ${SOURCE_PATH}/DirectXMesh/DirectXMesh.inl
+    DESTINATION ${CURRENT_PACKAGES_DIR}/include
+)
+file(INSTALL
+    ${SOURCE_PATH}/DirectXMesh/Bin/Desktop_2017/${BUILD_ARCH}/Debug/DirectXMesh.lib
+    DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
+file(INSTALL
+    ${SOURCE_PATH}/DirectXMesh/Bin/Desktop_2017/${BUILD_ARCH}/Release/DirectXMesh.lib
+    DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+
+set(TOOL_PATH ${CURRENT_PACKAGES_DIR}/tools)
+file(INSTALL
+    ${SOURCE_PATH}/Meshconvert/Bin/Desktop_2017/${BUILD_ARCH}/Release/Meshconvert.exe
+    DESTINATION ${TOOL_PATH})
+
+# Handle copyright
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/directxmesh)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/directxmesh/LICENSE ${CURRENT_PACKAGES_DIR}/share/directxmesh/copyright)
