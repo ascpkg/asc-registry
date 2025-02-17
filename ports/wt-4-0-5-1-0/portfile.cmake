c4f5a92a384b38aa1f29c8413509f6d6df067e67
Capric <capric8416@gmail.com>
2025-02-17 17:46:28 +0800

    flatten https://github.com/microsoft/vcpkg/commit/1dbb58814 from 47812810+wangli28@users.noreply.github.com at 2019-06-18 01:30:25 +0800

--- ports/wt-4-0-5-1-0/portfile.cmake
+++ ports/wt-4-0-5-1-0/portfile.cmake
@@ -10,6 +10,7 @@ vcpkg_from_github(
         0002-link-glew.patch
         0003-disable-boost-autolink.patch
         0004-link-ssl.patch
+		0005-XML_file_path.patch
 )
 
 string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" SHARED_LIBS)
@@ -18,6 +19,7 @@ vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
     PREFER_NINJA
     OPTIONS
+		-DINSTALL_CONFIG_FILE_PATH="${DOWNLOADS}/wt"
         -DSHARED_LIBS=${SHARED_LIBS}
         -DBOOST_DYNAMIC=ON
         -DDISABLE_BOOST_AUTOLINK=ON
@@ -42,14 +44,6 @@ vcpkg_configure_cmake(
 vcpkg_install_cmake()
 
 vcpkg_fixup_cmake_targets(CONFIG_PATH share/wt)
-
-# Fix absolute paths.
-file(GLOB CMAKE_FILES ${CURRENT_PACKAGES_DIR}/share/wt/*.cmake)
-foreach(CMAKE_FILE ${CMAKE_FILES})
-    file(READ ${CMAKE_FILE} _contents)
-    string(REPLACE "${VCPKG_ROOT_DIR}/installed/${TARGET_TRIPLET}" "\${_IMPORT_PREFIX}" _contents "${_contents}")
-    file(WRITE ${CMAKE_FILE} "${_contents}")
-endforeach()
 
 # There is no way to suppress installation of the headers and resource files in debug build.
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/debug/share)
