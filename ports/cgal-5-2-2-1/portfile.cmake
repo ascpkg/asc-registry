fdfdf816eda0fa11cc6603f70425c9e4c3cc3fdd
Capric <capric8416@gmail.com>
2025-02-17 18:56:27 +0800

    flatten https://github.com/microsoft/vcpkg/commit/0e1dc1218 from 83086508+strega-nil-ms@users.noreply.github.com at 2021-07-20 12:24:58 -0500

---
+++ ports/cgal-5-2-2-1/portfile.cmake
@@ -0,0 +1,56 @@
+vcpkg_buildpath_length_warning(37)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO CGAL/cgal
+    REF v5.2.2
+    SHA512 48c6f44f41d6cf4fead878d9e5d62325ec90fc1f744d547b2f5683e41e48b25d19db2606804ac302c24520600d1f5b37a1649c19891cd9e87e227480ef410dbc
+    HEAD_REF master
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        qt WITH_CGAL_Qt5
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DCGAL_HEADER_ONLY=ON
+        -DCGAL_INSTALL_CMAKE_DIR=share/cgal
+        ${FEATURE_OPTIONS}
+)
+
+vcpkg_install_cmake()
+
+vcpkg_fixup_cmake_targets()
+
+vcpkg_copy_pdbs()
+
+# Clean
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin)
+else()
+    foreach(ROOT ${CURRENT_PACKAGES_DIR}/bin)
+        file(REMOVE
+            ${ROOT}/cgal_create_CMakeLists
+            ${ROOT}/cgal_create_cmake_script
+            ${ROOT}/cgal_make_macosx_app
+        )
+    endforeach()
+endif()
+
+file(INSTALL ${SOURCE_PATH}/Installation/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
+
+file(
+    COPY
+        ${SOURCE_PATH}/Installation/LICENSE.BSL
+        ${SOURCE_PATH}/Installation/LICENSE.RFL
+        ${SOURCE_PATH}/Installation/LICENSE.GPL
+        ${SOURCE_PATH}/Installation/LICENSE.LGPL
+    DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT}
+)
+
+file(INSTALL ${CMAKE_CURRENT_LIST_DIR}/usage DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
