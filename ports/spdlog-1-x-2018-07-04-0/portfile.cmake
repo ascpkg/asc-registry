67fdfd8ed26fdd5cfba650534f3f3531198f0e42
Capric <capric8416@gmail.com>
2025-02-17 17:32:42 +0800

    flatten https://github.com/microsoft/vcpkg/commit/a382578f1 from roschuma@microsoft.com at 2018-07-04 18:54:04 -0700

---
+++ ports/spdlog-1-x-2018-07-04-0/portfile.cmake
@@ -0,0 +1,35 @@
+#header-only library
+include(vcpkg_common_functions)
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO gabime/spdlog
+    REF 59f54cda10ea1ee30003ef46f08371e5ed6dccb4
+    SHA512 2a80b5b83b59660c2feb87c959cb53628efbc37098cbd2b16f691f67b6c44239d0107987ecbcb45b443cf257dc729beba74f4dba6f43fc165710759d028c0787
+    HEAD_REF v1.x
+)
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+    OPTIONS
+        -DSPDLOG_BUILD_TESTING=OFF
+)
+
+vcpkg_install_cmake()
+
+# Move cmake files, ensuring they will be 3 directories up the import prefix
+file(MAKE_DIRECTORY ${CURRENT_PACKAGES_DIR}/share/spdlog)
+file(RENAME ${CURRENT_PACKAGES_DIR}/lib/cmake/spdlog/ ${CURRENT_PACKAGES_DIR}/share/spdlog/cmake)
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug)
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib)
+
+# use vcpkg-provided fmt library
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/include/spdlog/fmt/bundled)
+file(READ ${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h SPDLOG_TWEAKME_CONTENTS)
+string(REPLACE "// #define SPDLOG_FMT_EXTERNAL" "#define SPDLOG_FMT_EXTERNAL" SPDLOG_TWEAKME_CONTENTS "${SPDLOG_TWEAKME_CONTENTS}")
+file(WRITE ${CURRENT_PACKAGES_DIR}/include/spdlog/tweakme.h "${SPDLOG_TWEAKME_CONTENTS}")
+
+# Handle copyright
+file(COPY ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/spdlog)
+file(RENAME ${CURRENT_PACKAGES_DIR}/share/spdlog/LICENSE ${CURRENT_PACKAGES_DIR}/share/spdlog/copyright)
