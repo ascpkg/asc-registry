d2ba1b37c29b089a4d3b44f9419a8935b1f26725
Capric <capric8416@gmail.com>
2025-02-17 21:32:08 +0800

    flatten https://github.com/microsoft/vcpkg/commit/452ed95c1 from 65999885+FrankXie05@users.noreply.github.com at 2023-07-14 16:36:33 -0700

---
+++ ports/discordcoreapi-1-60-0/portfile.cmake
@@ -0,0 +1,31 @@
+if(VCPKG_TARGET_IS_LINUX)
+    message(WARNING "DiscordCoreAPI only supports g++ 11 on linux.")
+endif()
+
+vcpkg_from_github(
+	OUT_SOURCE_PATH SOURCE_PATH
+	REPO RealTimeChris/DiscordCoreAPI
+	REF "v${VERSION}"
+    	SHA512 e10bcf274b2487d117990a511b87d9ef15e390f23c5788ba34043167211a7182bbbb9c81ed0eb4f3a3435fa692401f3f7c4a6a15a2c7cbdcd2230d122d2ced13
+    	HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(NO_PREFIX_CORRECTION)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include" "${CURRENT_PACKAGES_DIR}/debug/share")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+file(
+	INSTALL "${SOURCE_PATH}/License.md"
+	DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}"
+	RENAME copyright
+)
