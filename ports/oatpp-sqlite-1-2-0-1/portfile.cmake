34e7d849227c6d6ae3812888a074813099ca0311
Capric <capric8416@gmail.com>
2025-02-17 18:29:23 +0800

    flatten https://github.com/microsoft/vcpkg/commit/bf7f7a116 from mheyman@gmail.com at 2020-11-05 21:21:17 -0500

--- ports/oatpp-sqlite-1-2-0-1/portfile.cmake
+++ ports/oatpp-sqlite-1-2-0-1/portfile.cmake
@@ -2,14 +2,12 @@ set(OATPP_VERSION "1.2.0")
 
 vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
-# get the source
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO oatpp/oatpp-libressl
-    REF cd2e9a515131e5e7dc043c591e952e12cd63db2c # 1.2.0
-    SHA512 e6d208edddff5373c07887b76fc808733bd363c340e740047ae90317874b73a5ef71e5cbbb0f9b1b48632c7a78709858a5ff0de81bc39207961e3642c0104010
+    REPO oatpp/oatpp-sqlite
+    REF 5128a4a1dc1f6ff5d0618237d1dbb9e9d5447e2f # 1.2.0
+    SHA512 d066701d4c857e895816db14c9700f321e171d2f990ee1c505c8b5243351d726db973d61ec7ebbe1b3f19e678627c41975aeac24fd2891d41900dfb25d427903
     HEAD_REF master
-    PATCHES "libress-submodule-downgrade-required-libressl-version.patch"
 )
 
 vcpkg_configure_cmake(
@@ -18,11 +16,10 @@ vcpkg_configure_cmake(
     OPTIONS
         "-DOATPP_BUILD_TESTS:BOOL=OFF"
         "-DCMAKE_CXX_FLAGS=-D_CRT_SECURE_NO_WARNINGS"
-        "-DLIBRESSL_ROOT_DIR=${CURRENT_INSTALLED_DIR}"       
+        "-DOATPP_SQLITE_AMALGAMATION:BOOL=OFF"
 )
-
 vcpkg_install_cmake()
-vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/oatpp-libressl-${OATPP_VERSION})
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/oatpp-sqlite-${OATPP_VERSION})
 vcpkg_copy_pdbs()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
