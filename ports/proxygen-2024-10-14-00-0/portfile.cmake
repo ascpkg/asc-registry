76660b319f38d5166c79afb162b43b1dfd9d9daf
Capric <capric8416@gmail.com>
2025-02-17 23:48:21 +0800

    flatten https://github.com/microsoft/vcpkg/commit/8c2f2d0dc from c8ef@outlook.com at 2024-10-22 12:42:19 +0800

---
+++ ports/proxygen-2024-10-14-00-0/portfile.cmake
@@ -0,0 +1,36 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebook/proxygen
+    REF "v${VERSION}"
+    SHA512 72b71ec08a40a7c037feee9d33a4b1cecaf44fc24d7b3896bd75ef920576d8413051e7fa0b7d5d4470c516850739127f269439af875d982a8d0420d06e2764c1
+    HEAD_REF main
+    PATCHES
+        remove-register.patch
+        fix-zstd-zlib-dependency.patch
+        fix-dependency.patch
+)
+
+vcpkg_find_acquire_program(PYTHON3)
+get_filename_component(PYTHON3_PATH "${PYTHON3}" DIRECTORY)
+vcpkg_add_to_path(${PYTHON3_PATH})
+
+vcpkg_add_to_path(PREPEND "${CURRENT_HOST_INSTALLED_DIR}/tools/gperf")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH ${SOURCE_PATH}
+)
+
+vcpkg_cmake_install()
+
+vcpkg_copy_tools(TOOL_NAMES hq proxygen_curl proxygen_echo proxygen_h3datagram_client proxygen_httperf2 proxygen_proxy proxygen_push proxygen_static AUTO_CLEAN)
+
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/proxygen)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
