7d7744fe486a7d7ae16026158b0113003676eabf
Capric <capric8416@gmail.com>
2025-02-17 19:46:59 +0800

    flatten https://github.com/microsoft/vcpkg/commit/37ebb4419 from eric.d.kilmer@gmail.com at 2022-04-19 12:29:01 -0400

---
+++ ports/z3-4-8-15-0/portfile.cmake
@@ -0,0 +1,37 @@
+vcpkg_find_acquire_program(PYTHON2)
+get_filename_component(PYTHON2_DIR "${PYTHON2}" DIRECTORY)
+vcpkg_add_to_path("${PYTHON2_DIR}")
+
+vcpkg_from_github(
+  OUT_SOURCE_PATH SOURCE_PATH
+  REPO Z3Prover/z3
+  REF z3-4.8.15
+  SHA512 7b08dec5b035a38edc90c4c491f508fd9ed227357de94400169db53d4c59382bd6a81ae6615771023a06534a3aa92668844f0ebfcc2a3b5ef4bba957426a0c6c
+  HEAD_REF master
+  PATCHES
+      fix-install-path.patch
+      remove-flag-overrides.patch
+      fix-32-bit-build.patch
+)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+  set(BUILD_STATIC "-DZ3_BUILD_LIBZ3_SHARED=OFF")
+endif()
+
+vcpkg_cmake_configure(
+  SOURCE_PATH ${SOURCE_PATH}
+  OPTIONS
+    ${BUILD_STATIC}
+    -DZ3_BUILD_TEST_EXECUTABLES=OFF
+    -DZ3_ENABLE_EXAMPLE_TARGETS=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/z3)
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/LICENSE.txt" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+
+vcpkg_fixup_pkgconfig()
