6e5bd70f7fa517075fbc375a4f31833f6ab45e98
Capric <capric8416@gmail.com>
2025-02-17 23:40:50 +0800

    flatten https://github.com/microsoft/vcpkg/commit/21c0fc5df from 82102418+Al17OTON@users.noreply.github.com at 2024-10-03 04:05:12 +0900

---
+++ ports/live555-2024-09-29-0/portfile.cmake
@@ -0,0 +1,36 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_download_distfile(ARCHIVE
+    URLS "http://live555.com/liveMedia/public/live.2024.09.29.tar.gz"
+    FILENAME "live.2024.09.29.tar.gz"
+    SHA512 af2d6585729b3254e0ffabd6f73d40806bffb71ddfa8e0a6ad103318fe0177fb1cfdd062e042e3be68a5e437619f5569fbe7272d02bd922f28d0ce2c33082712
+)
+
+vcpkg_extract_source_archive(
+    SOURCE_PATH
+    ARCHIVE "${ARCHIVE}"
+    PATCHES
+        fix-RTSPClient.patch
+        fix_operator_overload.patch
+)
+
+file(COPY "${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt" DESTINATION "${SOURCE_PATH}")
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+vcpkg_copy_pdbs()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-live555)
+
+file(GLOB HEADERS
+    "${SOURCE_PATH}/BasicUsageEnvironment/include/*.h*"
+    "${SOURCE_PATH}/groupsock/include/*.h*"
+    "${SOURCE_PATH}/liveMedia/include/*.h*"
+    "${SOURCE_PATH}/UsageEnvironment/include/*.h*"
+)
+
+file(COPY ${HEADERS} DESTINATION "${CURRENT_PACKAGES_DIR}/include")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/COPYING")
