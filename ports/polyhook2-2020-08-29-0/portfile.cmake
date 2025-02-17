f95983ab58c9c66e75faf37755c588bfd21a18a5
Capric <capric8416@gmail.com>
2025-02-17 18:22:21 +0800

    flatten https://github.com/microsoft/vcpkg/commit/19b706c9d from stevemk14ebr@gmail.com at 2020-08-31 12:26:58 -0400

---
+++ ports/polyhook2-2020-08-29-0/portfile.cmake
@@ -0,0 +1,52 @@
+vcpkg_fail_port_install(ON_ARCH "arm" "arm64" ON_TARGET "Linux" "OSX" "UWP")
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stevemk14ebr/PolyHook_2_0
+    REF  b3c0baf639614c4b4ca56e5df2e9da688986917b
+    SHA512 a1fb531fcbd7d6ac084efc7a67b7a1d9e661f0e770df02db7458a311c54ca1a1f11b8be0ae438a40259603414bfb2e64bab69d45754bc910363daa912e995550
+    HEAD_REF master
+)
+
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    capstone  POLYHOOK_DISASM_CAPSTONE
+    zydis     POLYHOOK_DISASM_ZYDIS
+    exception POLYHOOK_FEATURE_EXCEPTION
+    detours   POLYHOOK_FEATURE_DETOURS
+    inlinentd POLYHOOK_FEATURE_INLINENTD
+    pe        POLYHOOK_FEATURE_PE
+    virtuals  POLYHOOK_FEATURE_VIRTUALS
+)
+
+if (VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    set(BUILD_SHARED_LIB OFF)
+else()
+    set(BUILD_SHARED_LIB ON)
+endif()
+
+if (VCPKG_CRT_LINKAGE STREQUAL "static")
+    set(BUILD_STATIC_RUNTIME ON)
+else()
+    set(BUILD_STATIC_RUNTIME OFF)
+endif()
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA  
+    OPTIONS ${FEATURE_OPTIONS}
+      -DPOLYHOOK_BUILD_SHARED_LIB=${BUILD_SHARED_LIB}
+      -DPOLYHOOK_BUILD_STATIC_RUNTIME=${BUILD_STATIC_RUNTIME}
+      -DPOLYHOOK_USE_EXTERNAL_ASMJIT=ON
+      -DPOLYHOOK_USE_EXTERNAL_CAPSTONE=ON
+      -DPOLYHOOK_USE_EXTERNAL_ZYDIS=ON
+)
+
+vcpkg_install_cmake()
+
+file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+
+vcpkg_copy_pdbs()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/PolyHook_2 TARGET_PATH share/PolyHook_2)
+
+# Handle copyright
+file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT} RENAME copyright)
