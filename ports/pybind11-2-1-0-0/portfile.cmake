02b31aaf95dee3fb826b054230291c8589e3103f
Capric <capric8416@gmail.com>
2025-02-17 17:21:32 +0800

    flatten https://github.com/microsoft/vcpkg/commit/2aff607cb from roschuma@microsoft.com at 2017-03-27 17:25:38 -0700

--- ports/pybind11-2-1-0-0/portfile.cmake
+++ ports/pybind11-2-1-0-0/portfile.cmake
@@ -1,6 +1,6 @@
 include(vcpkg_common_functions)
 
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/pybind11-2.0.1)
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/pybind11-2.1.0)
 
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/pybind/pybind11/archive/v2.1.0.tar.gz"
@@ -9,22 +9,14 @@ vcpkg_download_distfile(ARCHIVE
 )
 vcpkg_extract_source_archive(${ARCHIVE})
 
-# link the MSVC runtime statically if set.
-if(VCPKG_CRT_LINKAGE STREQUAL dynamic)
-    set(CRUNTIME /MD)
-else()
-    set(CRUNTIME /MT)
-endif()
-
 vcpkg_configure_cmake(
         SOURCE_PATH ${SOURCE_PATH}
-        OPTIONS
+        OPTIONS -DPYBIND11_TEST=OFF
 )
 
 vcpkg_install_cmake()
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/)
-
 
 # copy license
 file(INSTALL ${SOURCE_PATH}/LICENSE DESTINATION ${CURRENT_PACKAGES_DIR}/share/pybind11/copyright)
