55aa6df1b8472f61c819d0dc3069696cf9d31cf4
Capric <capric8416@gmail.com>
2025-02-17 17:46:54 +0800

    flatten https://github.com/microsoft/vcpkg/commit/25f628e05 from 47264268+JackBoosY@users.noreply.github.com at 2019-06-25 01:46:37 +0800

--- ports/flann-1-9-1-0/portfile.cmake
+++ ports/flann-1-9-1-0/portfile.cmake
@@ -1,38 +1,36 @@
-# Common Ambient Variables:
-#   CURRENT_BUILDTREES_DIR    = ${VCPKG_ROOT_DIR}\buildtrees\${PORT}
-#   CURRENT_PACKAGES_DIR      = ${VCPKG_ROOT_DIR}\packages\${PORT}_${TARGET_TRIPLET}
-#   CURRENT_PORT DIR          = ${VCPKG_ROOT_DIR}\ports\${PORT}
-#   PORT                      = current port name (zlib, etc)
-#   TARGET_TRIPLET            = current triplet (x86-windows, x64-windows-static, etc)
-#   VCPKG_CRT_LINKAGE         = C runtime linkage type (static, dynamic)
-#   VCPKG_LIBRARY_LINKAGE     = target library linkage type (static, dynamic)
-#   VCPKG_ROOT_DIR            = <C:\path\to\current\vcpkg>
-#   VCPKG_TARGET_ARCHITECTURE = target architecture (x64, x86, arm)
-#
-
 include(vcpkg_common_functions)
-set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/flann-1.9.1)
-vcpkg_download_distfile(ARCHIVE
-    URLS "https://github.com/mariusmuja/flann/archive/1.9.1.zip"
-    FILENAME "flann-1.9.1.zip"
-    SHA512 d2f5c13535a179800602dc8a94ee91da23b01f71bc893facdf91ab18a73c5738604cda9870f38c3797af75ded47c808b1d95d3bde707af814e1eb1388b56bb95
+
+#the port uses inside the CMAKE_WINDOWS_EXPORT_ALL_SYMBOLS, which is discouraged by vcpkg.
+#Since it's its author choice, we should not disallow it, but unfortunately looks like it's broken, so we block it anyway...
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO mariusmuja/flann
+    REF  1d04523268c388dabf1c0865d69e1b638c8c7d9d
+    SHA512 61e322222c2daa0b9677095e5ca231cba7c305ce754ad8e659eee350111c1e04351181c3af04e45ab6e5c9edea49c7b9ec6499bb0dbc080b87af36eb11c6ef7c
+    HEAD_REF master
+    PATCHES fix-build-error.patch
 )
-vcpkg_extract_source_archive(${ARCHIVE})
 
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
-    PREFER_NINJA # Disable this option if project cannot be built with Ninja
-    OPTIONS -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=ON -DBUILD_EXAMPLES=OFF -DBUILD_DOC=OFF -DBUILD_PYTHON_BINDINGS=OFF -DBUILD_MATLAB_BINDINGS=OFF
+    PREFER_NINJA
+    OPTIONS
+        -DBUILD_EXAMPLES=OFF
+        -DBUILD_TESTS=OFF
+        -DBUILD_DOC=OFF
+        -DBUILD_PYTHON_BINDINGS=OFF
+        -DBUILD_MATLAB_BINDINGS=OFF
+        -DCMAKE_DEBUG_POSTFIX=d
+        -DHDF5_NO_FIND_PACKAGE_CONFIG_FILE=ON
 )
 
 vcpkg_install_cmake()
+vcpkg_fixup_cmake_targets(CONFIG_PATH lib/cmake/${PORT})
 
 file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-endif()
-
 # Handle copyright
-file(COPY ${CURRENT_BUILDTREES_DIR}/src/flann-1.9.1/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/flann)
+file(COPY ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/flann)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/flann/COPYING ${CURRENT_PACKAGES_DIR}/share/flann/copyright)
