2c62c83d8d826ff3a58586e0428f71336a2d30bb
Capric <capric8416@gmail.com>
2025-02-17 18:28:27 +0800

    flatten https://github.com/microsoft/vcpkg/commit/318a8adbd from stoperro@users.noreply.github.com at 2020-10-28 22:22:51 +0100

--- ports/boost-fiber-1-73-0-2/portfile.cmake
+++ ports/boost-fiber-1-73-0-2/portfile.cmake
@@ -2,11 +2,10 @@
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO boostorg/context
+    REPO boostorg/fiber
     REF boost-1.73.0
-    SHA512 cb460e9d7f5b2782164e49820b097402f6a443718d689b2f793f5ab7030012a80418dfc51154f299c9f987d6864d87f7536476ffa97da1089bf6b70f2990ebea
+    SHA512 25d129e4228f3517abc641f6858572b3196c2d79945013aa45d61a544c094e83e0103c9aba04e54fc07e53d2e29379d60a0db9a1c995dcccaef2ba2b5cda94d9
     HEAD_REF master
-    PATCHES fix_exports_for_32bit_GNU_asm_for_windows.patch
 )
 
 file(READ "${SOURCE_PATH}/build/Jamfile.v2" _contents)
@@ -18,9 +17,3 @@ include(${CURRENT_INSTALLED_DIR}/share/boost-build/boost-modular-build.cmake)
 boost_modular_build(SOURCE_PATH ${SOURCE_PATH})
 include(${CURRENT_INSTALLED_DIR}/share/boost-vcpkg-helpers/boost-modular-headers.cmake)
 boost_modular_headers(SOURCE_PATH ${SOURCE_PATH})
-
-# boost-context removed all.hpp, which is used by FindBoost to determine that context is installed
-if(NOT EXISTS ${CURRENT_PACKAGES_DIR}/include/boost/context/all.hpp)
-    file(WRITE ${CURRENT_PACKAGES_DIR}/include/boost/context/all.hpp
-        "#error \"#include <boost/context/all.hpp> is no longer supported by boost_context.\"")
-endif()
--- ports/boost-log-1-73-0-2/portfile.cmake
+++ ports/boost-log-1-73-0-2/portfile.cmake
@@ -2,25 +2,26 @@
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO boostorg/context
+    REPO boostorg/log
     REF boost-1.73.0
-    SHA512 cb460e9d7f5b2782164e49820b097402f6a443718d689b2f793f5ab7030012a80418dfc51154f299c9f987d6864d87f7536476ffa97da1089bf6b70f2990ebea
+    SHA512 150013753d5060618c431ed9a819e3ec666bdba42cf3d463335ca49d66b8d6e800d5419332011f5ce05f19bc9c67a1cebc547279bf2931258ff7753a67796f2f
     HEAD_REF master
-    PATCHES fix_exports_for_32bit_GNU_asm_for_windows.patch
 )
 
 file(READ "${SOURCE_PATH}/build/Jamfile.v2" _contents)
 string(REPLACE "import ../../config/checks/config" "import config/checks/config" _contents "${_contents}")
+string(REPLACE " <conditional>@select-arch-specific-sources" "#<conditional>@select-arch-specific-sources" _contents "${_contents}")
 file(WRITE "${SOURCE_PATH}/build/Jamfile.v2" "${_contents}")
 file(COPY "${CURRENT_INSTALLED_DIR}/share/boost-config/checks" DESTINATION "${SOURCE_PATH}/build/config")
+
+file(READ ${SOURCE_PATH}/build/log-architecture.jam _contents)
+string(REPLACE
+    "\nproject.load [ path.join [ path.make $(here:D) ] ../../config/checks/architecture ] ;"
+    "\nproject.load [ path.join [ path.make $(here:D) ] config/checks/architecture ] ;"
+    _contents "${_contents}")
+file(WRITE ${SOURCE_PATH}/build/log-architecture.jam "${_contents}")
 
 include(${CURRENT_INSTALLED_DIR}/share/boost-build/boost-modular-build.cmake)
 boost_modular_build(SOURCE_PATH ${SOURCE_PATH})
 include(${CURRENT_INSTALLED_DIR}/share/boost-vcpkg-helpers/boost-modular-headers.cmake)
 boost_modular_headers(SOURCE_PATH ${SOURCE_PATH})
-
-# boost-context removed all.hpp, which is used by FindBoost to determine that context is installed
-if(NOT EXISTS ${CURRENT_PACKAGES_DIR}/include/boost/context/all.hpp)
-    file(WRITE ${CURRENT_PACKAGES_DIR}/include/boost/context/all.hpp
-        "#error \"#include <boost/context/all.hpp> is no longer supported by boost_context.\"")
-endif()
