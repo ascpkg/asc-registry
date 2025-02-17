5313ae81c82da42f10681ff3e32a23a796b7c9e9
Capric <capric8416@gmail.com>
2025-02-17 17:23:54 +0800

    flatten https://github.com/microsoft/vcpkg/commit/73a02b9f7 from me@mixaill.tk at 2017-08-19 23:29:24 +0300

---
+++ ports/lcms-2-8-2-0/portfile.cmake
@@ -0,0 +1,32 @@
+include(vcpkg_common_functions)
+
+vcpkg_from_github( 
+    OUT_SOURCE_PATH SOURCE_PATH 
+    REPO mm2/Little-CMS
+    REF lcms2.8
+    SHA512 ad904ce8acead6c8e255feb8386c1ab3fa432c3b36a3b521bc5c50993cb47ce4d42be0ad240dd8dd3bfeb3c0e884d8184f58797da5ef297b2f9a0e7da9788644
+    HEAD_REF master 
+) 
+
+file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
+
+vcpkg_configure_cmake(
+    SOURCE_PATH ${SOURCE_PATH}
+    PREFER_NINJA
+	OPTIONS_DEBUG
+        -DSKIP_INSTALL_HEADERS=ON
+)
+
+vcpkg_build_cmake()
+vcpkg_install_cmake()
+
+file(INSTALL ${SOURCE_PATH}/COPYING DESTINATION ${CURRENT_PACKAGES_DIR}/share/lcms RENAME copyright)
+
+vcpkg_copy_pdbs()
+
+#patch header files to fix import/export issues
+if(VCPKG_LIBRARY_LINKAGE STREQUAL dynamic)
+  vcpkg_apply_patches(
+    SOURCE_PATH ${CURRENT_PACKAGES_DIR}/include
+    PATCHES "${CMAKE_CURRENT_LIST_DIR}/shared.patch")
+endif()
