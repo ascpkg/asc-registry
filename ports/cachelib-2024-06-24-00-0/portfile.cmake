2bfd01723e670955b20165ade99ea34201e7715b
Capric <capric8416@gmail.com>
2025-02-17 23:13:03 +0800

    flatten https://github.com/microsoft/vcpkg/commit/c7e34d4f9 from c8ef@outlook.com at 2024-06-27 11:18:37 +0800

---
+++ ports/cachelib-2024-06-24-00-0/portfile.cmake
@@ -0,0 +1,39 @@
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebook/CacheLib
+    REF "v${VERSION}"
+    SHA512 8463c449c798e139b83e8be7716e613e38e654247cb430708e718c38c0d67fbd0b51c02aadaa1909325d0633ff3617ab61b9ddbf44060930ff79107681a6c90a
+    HEAD_REF main
+    PATCHES
+        fix-build.patch
+        fix-glog.patch
+)
+
+FIND_PATH(NUMA_INCLUDE_DIR NAME numa.h
+    PATHS ENV NUMA_ROOT
+    HINTS "$ENV{HOME}/local/include" /opt/local/include /usr/local/include /usr/include
+)
+
+IF (NOT NUMA_INCLUDE_DIR)
+    MESSAGE(FATAL_ERROR "Numa library not found.\nTry: 'sudo yum install numactl numactl-devel' (or sudo apt-get install libnuma1 libnuma-dev)")
+ENDIF ()
+
+file(REMOVE "${SOURCE_PATH}/cmake/FindGlog.cmake")
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/cachelib"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DCMAKE_INSTALL_DIR=share/cachelib
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH share/cachelib PACKAGE_NAME cachelib)
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
+
+vcpkg_fixup_pkgconfig()
---
+++ ports/wangle-2024-06-24-00-0/portfile.cmake
@@ -0,0 +1,52 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebook/wangle
+    REF "v${VERSION}"
+    SHA512 a93d367e6061ef37b7775aa99fdfdba3c13abea485d8968c1f593bc456ee19519dd26aa52dae119b01296566f76114e01c4dad50031fe36485060feb338c095e
+    HEAD_REF main
+    PATCHES
+        fix-config-cmake.patch
+        fix_dependency.patch
+)
+
+file(REMOVE
+  "${SOURCE_PATH}/wangle/cmake/FindDoubleConversion.cmake"
+  "${SOURCE_PATH}/build/fbcode_builder/CMake/FindGflags.cmake"
+  "${SOURCE_PATH}/build/fbcode_builder/CMake/FindGlog.cmake"
+  "${SOURCE_PATH}/build/fbcode_builder/CMake/FindGMock.cmake"
+  "${SOURCE_PATH}/build/fbcode_builder/CMake/FindLibEvent.cmake"
+  "${SOURCE_PATH}/build/fbcode_builder/CMake/FindSodium.cmake"
+  "${SOURCE_PATH}/build/fbcode_builder/CMake/FindZstd.cmake"
+)
+
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/wangle"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DBUILD_EXAMPLES=OFF
+        -DINCLUDE_INSTALL_DIR:STRING=include
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/wangle)
+
+file(READ "${CURRENT_PACKAGES_DIR}/share/wangle/wangle-targets.cmake" _contents)
+STRING(REPLACE "\${_IMPORT_PREFIX}/lib/" "\${_IMPORT_PREFIX}/\$<\$<CONFIG:DEBUG>:debug/>lib/" _contents "${_contents}")
+STRING(REPLACE "\${_IMPORT_PREFIX}/debug/lib/" "\${_IMPORT_PREFIX}/\$<\$<CONFIG:DEBUG>:debug/>lib/" _contents "${_contents}")
+file(WRITE "${CURRENT_PACKAGES_DIR}/share/wangle/wangle-targets.cmake" "${_contents}")
+
+vcpkg_copy_pdbs()
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/include/wangle/util/test"
+    "${CURRENT_PACKAGES_DIR}/include/wangle/ssl/test/certs"
+    "${CURRENT_PACKAGES_DIR}/include/wangle/service/test"
+    "${CURRENT_PACKAGES_DIR}/include/wangle/deprecated/rx/test"
+)
+
+file(INSTALL "${CURRENT_PORT_DIR}/usage" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}")
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
