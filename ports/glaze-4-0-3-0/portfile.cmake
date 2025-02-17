a2560467959150a7dcc3a8880d804b8eb57f3c60
Capric <capric8416@gmail.com>
2025-02-18 00:03:44 +0800

    flatten https://github.com/microsoft/vcpkg/commit/6a4692e70 from 359941+mikaellindemann@users.noreply.github.com at 2024-12-13 15:48:08 +0100

---
+++ ports/glaze-4-0-3-0/portfile.cmake
@@ -0,0 +1,25 @@
+if(VCPKG_TARGET_IS_LINUX)
+    message("Warning: `glaze` requires Clang15+ or GCC 12+ on Linux")
+endif()
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO stephenberry/glaze
+    REF "v${VERSION}"
+    SHA512 31e736069a877908f86aec1c5d9263475dd2d8efd2af8d5cb71ec6531a15a96ccae6de0799830ba577546bcbaee3d59672002ba61e6bc6d8e2ca723dd596083e
+    HEAD_REF main
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -Dglaze_DEVELOPER_MODE=OFF
+        -Dglaze_BUILD_EXAMPLES=OFF
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup()
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
