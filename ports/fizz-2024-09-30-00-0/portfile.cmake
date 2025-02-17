654b6d762df54be6e0e653aad1979f5a9451e5d9
Capric <capric8416@gmail.com>
2025-02-17 23:40:24 +0800

    flatten https://github.com/microsoft/vcpkg/commit/1d830962c from c8ef@outlook.com at 2024-10-02 22:03:05 +0800

---
+++ ports/fizz-2024-09-30-00-0/portfile.cmake
@@ -0,0 +1,43 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO facebookincubator/fizz
+    REF "v${VERSION}"
+    SHA512 9ad53ab9756ef614113c816773d73312da01533b26037a6fa071226a955751c8f30f18c40c876884f50b7d9899260dfa50a9ea035a89c04471e313392d6b8957
+    HEAD_REF main
+    PATCHES
+        fix-build.patch
+)
+
+# Prefer installed config files
+file(REMOVE
+    "${SOURCE_PATH}/fizz/cmake/FindGMock.cmake"
+    "${SOURCE_PATH}/fizz/cmake/FindGflags.cmake"
+    "${SOURCE_PATH}/fizz/cmake/FindGlog.cmake"
+    "${SOURCE_PATH}/fizz/cmake/FindLibevent.cmake"
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}/fizz"
+    OPTIONS
+        -DBUILD_TESTS=OFF
+        -DBUILD_EXAMPLES=OFF
+        -DINCLUDE_INSTALL_DIR:STRING=include
+)
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(CONFIG_PATH lib/cmake/fizz)
+vcpkg_copy_pdbs()
+
+vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/share/fizz/fizz-config.cmake" "lib/cmake/fizz" "share/fizz")
+
+file(REMOVE_RECURSE
+    "${CURRENT_PACKAGES_DIR}/debug/include"
+    "${CURRENT_PACKAGES_DIR}/include/fizz/crypto/aead/test/facebook"
+    "${CURRENT_PACKAGES_DIR}/include/fizz/record/test/facebook"
+    "${CURRENT_PACKAGES_DIR}/include/fizz/server/test/facebook"
+    "${CURRENT_PACKAGES_DIR}/include/fizz/tool/test"
+    "${CURRENT_PACKAGES_DIR}/include/fizz/util/test")
+
+vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
